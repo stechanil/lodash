48276e540bca1b2d6f9bb4671eb6be8cc5ef41cd
Anil Chauhan <anilchauhan@anilss691.local>
2024-05-07 15:13:26 +0530

    ci: sample security issues

--- src/demo/bugs.ts
+++ src/demo/bugs.ts
@@ -16,7 +16,7 @@ function complexFunction() {
 
 function unusedVariable() {
     const x = 10; // This variable is unused
-    const y = person?.name;
+    const y = person && person.name;
 }
 
 function vulnerableCode(userInput: string) {
